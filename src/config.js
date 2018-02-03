b2c9ac42f3ad3b3ab6ec97b9b6992543545105d8
Patrick Laplante <laplante.patrick@gmail.com>
2018-02-02 22:40:40 -0500

    First

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  cognito: {
+    USER_POOL_ID: "us-east-1_75YbjIjDN",
+    APP_CLIENT_ID: "574taeqhpe3c2oh6350hstkdo0",
+    REGION: "us-east-1",
+    IDENTITY_POOL_ID: "us-east-1:61af9286-a459-4d5a-97e8-14461bdafb9b",
+  },
+  apiGateway: {
+    URL: "https://yq9q4rytd6.execute-api.us-east-1.amazonaws.com/prod",
+    REGION: "us-east-1"
+  },
+  s3: {
+    BUCKET: "notes-app-uploads-coc"
+  },
+};
+
